c0c6962baf63a029b6af7f5dbfe7b636eafe18f4
piperfresh <sulainoabass@gmail.com>
2022-07-02 15:02:00 +0100

    hello word

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('i am rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Image(
+        image: AssetImage('images/diamond.jpg'),
+      ),
+    ),
+  ));
+}
